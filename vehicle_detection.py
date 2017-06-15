ec8fd6c854be1b03ca341fc4c67febcfcc854017
Ansgar Nell <ansgar.nell@gmail.com>
2017-06-15 12:30:44 +0200

    adde heatmaps

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -4,6 +4,8 @@ import cv2
 from lesson_functions import *
 import os
 import glob
+from scipy.ndimage.measurements import label
+from collections import deque
 
 dist_pickle = pickle.load( open("svc_pickle.p", "rb" ) )
 svc = dist_pickle["svc"]
@@ -16,9 +18,41 @@ hist_bins = dist_pickle["hist_bins"]
 colorspace = dist_pickle["colorspace"]
 hog_channel = dist_pickle["hog_channel"]
 
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap# Iterate through list of bboxes
+    
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
+
 # Define a single function that can extract features using hog sub-sampling and make predictions
-def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, hog_channel):
+def find_cars(img):
 	
+	rectangles = []
 	draw_img = np.copy(img)
 	img = img.astype(np.float32)/255
 	
@@ -92,7 +126,25 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
 				ytop_draw = np.int(ytop*scale)
 				win_draw = np.int(window*scale)
 				cv2.rectangle(draw_img,(xbox_left, ytop_draw+ystart),(xbox_left+win_draw,ytop_draw+win_draw+ystart),(255,0,0),6) 
+				rectangles.append(((xbox_left, ytop_draw+ystart),(xbox_left+win_draw,ytop_draw+win_draw+ystart)))
 				
+	return draw_img, rectangles
+	
+def pipeline(img):
+
+	global heatmaps
+
+	heat = np.zeros_like(img[:,:,0]).astype(np.float)
+	converted_image = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+	out_img, rectangles = find_cars(converted_image)
+	heat = add_heat(heat, rectangles)
+	heatmaps.append(heat)
+	heat = sum(heatmaps)/len(heatmaps)
+	# Apply threshold to help remove false positives
+	heat = apply_threshold(heat,6)
+	# Find final boxes from heatmap using label function
+	labels = label(heat)
+	draw_img = draw_labeled_bboxes(np.copy(img), labels)
 	return draw_img
 	
 ystart = 400
@@ -104,10 +156,27 @@ input_folder = './project_video/'
 images = glob.glob(input_folder + 'filename*.jpg')
 #input_folder = './test_images/'
 #images = glob.glob(input_folder + 'test*.jpg')
+"""
 for image in images:
 	filename = os.path.basename(image)
 	print('Processing file', filename)
 	img = cv2.imread(image)
-	out_img = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, hog_channel)
+	out_img, rectangles = find_cars(img)
 	write_name = input_folder + 'output_' + filename
 	cv2.imwrite(write_name, out_img)
+"""
+
+heatmaps = deque(maxlen=15)
+
+# Import everything needed to edit/save/watch video clips
+from moviepy.editor import VideoFileClip
+
+white_output = 'test_video_result.mp4'
+## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+## To do so add .subclip(start_second,end_second) to the end of the line below
+## Where start_second and end_second are integer values representing the start and end of the subclip
+## You may also uncomment the following line for a subclip of the first 5 seconds
+clip1 = VideoFileClip("project_video.mp4").subclip(20,25)
+#clip1 = VideoFileClip("project_video.mp4")
+white_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
+white_clip.write_videofile(white_output, audio=False)
